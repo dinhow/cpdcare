07a7c8f64779a1f16022a38b7c555ce88a553765
dinhow <dinhow.exe@gmail.com>
2022-01-25 20:21:17 -0300

    V 0.2.2

--- src/esp32_ota.ino
+++ src/esp32_ota.ino
@@ -11,8 +11,8 @@ const char * password = "@enzo1234";
 String FirmwareVer = {
   "2.2"
 };
-#define URL_fw_Version "https://raw.githubusercontent.com/programmer131/ESP8266_ESP32_SelfUpdate/master/esp32_ota/bin_version.txt"
-#define URL_fw_Bin "https://raw.githubusercontent.com/programmer131/ESP8266_ESP32_SelfUpdate/master/esp32_ota/fw.bin"
+#define URL_fw_Version "https://github.com/dinhow/cpdcare/raw/main/src/bin_version.txt"
+#define URL_fw_Bin "https://raw.githubusercontent.com/dinhow/cpdcare/main/src/fw.bin"
 
 //#define URL_fw_Version "http://cade-make.000webhostapp.com/version.txt"
 //#define URL_fw_Bin "http://cade-make.000webhostapp.com/firmware.bin"
